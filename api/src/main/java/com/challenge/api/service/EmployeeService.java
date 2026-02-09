8c79ea10158f16f831af59de18cc323966b8cd7b
cHuNgStEn <ipragmatic2r@gmail.com>
2026-02-09 15:24:10 -0500

    forgot to import Service from Springframework

--- api/src/main/java/com/challenge/api/service/EmployeeService.java
+++ api/src/main/java/com/challenge/api/service/EmployeeService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
