6f0c1a3cab1249f5c7aa518d0eb14a5801dec522
cHuNgStEn <ipragmatic2r@gmail.com>
2026-02-09 17:59:46 -0500

    implemented createEmployee endpoint

--- api/src/main/java/com/challenge/api/controller/EmployeeController.java
+++ api/src/main/java/com/challenge/api/controller/EmployeeController.java
@@ -1,15 +1,17 @@
 package com.challenge.api.controller;
 
 import com.challenge.api.model.Employee;
+import com.challenge.api.model.EmployeeDTO;
 import com.challenge.api.service.EmployeeService;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -43,12 +45,23 @@ public class EmployeeController {
     }
 
     /**
-     * @implNote Need not be concerned with an actual persistence layer.
      * @param requestBody hint!
      * @return Newly created Employee
      */
     @PostMapping
-    public Employee createEmployee(Object requestBody) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    public Employee createEmployee(@RequestBody EmployeeDTO request) {
+        if (request.getFirstName() == null
+                || request.getLastName() == null
+                || request.getSalary() == null
+                || request.getAge() == null
+                || request.getJobTitle() == null
+                || request.getEmail() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "All fields need to be non-null");
+        }
+
+        if (request.getContractHireDate() == null) {
+            request.setContractHireDate(Instant.now());
+        }
+        return employeeService.createEmployee(request);
     }
 }
